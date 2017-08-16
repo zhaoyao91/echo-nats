1fa94e68f13c2d969fc9e35272be3ae9c900fc50
zhaoyao <zhaoyao@nibirutech.com>
2017-08-16 11:04:38 +0800

    add logs for all events

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const {NATS_URL, SUBJECT} = process.env
 
 const natsClient = nats.connect(NATS_URL)
 
-natsClient.on('error', err => logger.error(err))
-
-natsClient.on('connect', () => {
-  logger.info({msg: 'nats connected', url: NATS_URL})
-})
+natsClient
+  .on('error', err => logger.error(err))
+  .on('connect', () => logger.info({msg: 'connected', url: NATS_URL}))
+  .on('disconnect', () => logger.info('disconnected'))
+  .on('reconnecting', () => logger.info('reconnecting'))
+  .on('close', () => logger.info('closed'))
 
 natsClient.subscribe(SUBJECT, (message, reply, subject) => {
   logger.info({msg: 'message received', message: {subject, content: message, reply}})
